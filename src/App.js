83bca2b23a62d16fb50765509ef2574088851752
RAVI KUMAR <ravikumartakajm@gmail.com>
2023-09-12 23:33:18 +0530

    Update app css & js

--- src/App.js
+++ src/App.js
@@ -1,57 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Routers, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout.js";
+import ShoppingContainer from "./components/ShoppingContainer";
+import ItemDetails from "./components/ItemDetails";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Routers>
+      <Routes>
+        <Route
+          path="/"
+          element={<Layout />}
+        >
+          <Route
+            path="/"
+            element={<ShoppingContainer />}
+          />
+          <Route
+            path="/item-details/:id"
+            element={<ItemDetails />}
+          />
+        </Route>
+      </Routes>
+    </Routers>
   );
 }
 
