a9180bb48d6b02fe61b6911050662ed6958201ae
RAVI KUMAR <ravikumartakajm@gmail.com>
2023-09-18 23:26:00 +0530

    Add Item detail and Shopping item Page

---
+++ src/components/ItemDetails.js
@@ -0,0 +1,57 @@
+import { useParams } from "react-router-dom";
+// import { add } from "../features/cart/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { addCartAsync } from "../features/cart/cartSlice";
+import { useEffect } from "react";
+import { getProductByIdAsync } from "../features/products/productsSlice";
+import NotFound from "./NotFound";
+
+const ItemDetails = () => {
+  const dispatch = useDispatch();
+  const { item, loading } = useSelector((state) => state.products);
+  const { id } = useParams();
+  useEffect(() => {
+    dispatch(getProductByIdAsync(id));
+  }, []);
+  const firstImageSrc = item && item.images && item.images[0];
+
+  if (id > '30') {
+    return <NotFound />;
+  } else {
+    return loading ? (
+      <p className="section grid lg:w-[70%] md:w-[75%] lg:grid-cols-3 md:grid-cols-2 gap-6">
+        Loading...
+      </p>
+    ) : (
+      <div className="min-h-screen w-full flex items-center justify-center p-4">
+        <div className="lg:flex items-center justify-center">
+          <img
+            src={firstImageSrc}
+            alt="image"
+            className="lg:w-[22rem] w-[200px] me-8"
+          />
+          <div>
+            <div className="mt-4 text-3xl font-extrabold mb-4">
+              {item.title}
+            </div>
+            <div className="mb-4">${item.price}</div>
+            <p className="max-w-[400px] mb-4">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Similique recusandae reiciendis, ad iure accusantium magni,
+              quisquam nisi asperiores incidunt quibusdam dolores deleniti.
+              Fugiat, nisi magnam.
+            </p>
+            <button
+              className="bg-black text-white p-3 mb-4"
+              onClick={() => dispatch(addCartAsync({ ...item, quantity: 1 }))}
+            >
+              Add To Cart
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default ItemDetails;
---
+++ src/components/ShoppingItem.js
@@ -0,0 +1,36 @@
+import { addCartAsync } from "../features/cart/cartSlice";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+
+const ShoppingItem = ({ item }) => {
+  const dispatch = useDispatch();
+  const { id, images, price, title } = item;
+
+  return (
+    <div>
+      <Link to={`/item-details/${id}`}>
+        <div className="bg-grey h-[400px] flex items-center justify-center">
+          <img
+            src={images[0]}
+            alt="image"
+            className="w-[200px]"
+          />
+        </div>
+      </Link>
+      <div className="mt-6 flex justify-between items-center px-4">
+        <div>
+          <div className="text-sm font-bold mb-3">{title}</div>
+          <div className="text-xl font-bold">${price}</div>
+        </div>
+        <button
+          className="bg-grey p-3 font-bold"
+          onClick={() => dispatch(addCartAsync({ ...item, quantity: 1  }))}
+        >
+          Add To Cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ShoppingItem;
