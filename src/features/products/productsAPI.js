606ea86cf459e5d335540b0dae3c3dbed25dd083
RAVI KUMAR <ravikumartakajm@gmail.com>
2023-09-18 23:31:04 +0530

    Add get product by id api

--- src/features/products/productsAPI.js
+++ src/features/products/productsAPI.js
@@ -1,3 +1,4 @@
 import { api } from "../../api/axios";
 
 export const getProductItems = () => api.get("/products");
+export const getProductItemById = (id) => api.get(`/products/${id}`);
--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -1,8 +1,9 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { getProductItems } from "./productsAPI";
+import { getProductItems, getProductItemById } from "./productsAPI";
 
 const initialState = {
   items: [],
+  item: [],
   loading: false,
   error: null,
 };
@@ -12,12 +13,18 @@ export const getProductsAsync = createAsyncThunk("products/get/status", async ()
   return res.data;
 });
 
+export const getProductByIdAsync = createAsyncThunk("product-by-id/get/status", async (id) => {
+  const res = await getProductItemById(id);
+  return res.data;
+});
+
 export const productsSlice = createSlice({
   name: "products",
   initialState,
   // reducers: {},
   extraReducers: (builder) => {
     builder
+      // Products
       .addCase(getProductsAsync.pending, (state) => {
         state.loading = true;
       })
@@ -28,7 +35,19 @@ export const productsSlice = createSlice({
       .addCase(getProductsAsync.rejected, (state, action) => {
         state.loading = false;
         state.error = action.error;
-      });
+      })
+      // Product by id
+      .addCase(getProductByIdAsync.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getProductByIdAsync.fulfilled, (state, action) => {
+        state.loading = false;
+        state.item = action.payload;
+      })
+      .addCase(getProductByIdAsync.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error;
+      })
   },
 });
 
