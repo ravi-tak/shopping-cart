f26f07a2bcfcfe823884f61a8b4c193ffe5ad690
RAVI KUMAR <ravikumartakajm@gmail.com>
2023-09-18 23:24:22 +0530

    Add not found page

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div>
+      <p className="section grid lg:w-[70%] md:w-[75%] lg:grid-cols-3 md:grid-cols-2 gap-6">
+        404: Page Not Found
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
